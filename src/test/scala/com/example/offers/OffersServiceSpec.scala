db142cf2fa8610f314cc60721376fb173b70f36d
William Lea <wjlea93@gmail.com>
2018-04-05 17:08:29 +0100

    Starting create offer tests

--- src/test/scala/com/example/offers/OffersServiceSpec.scala
+++ src/test/scala/com/example/offers/OffersServiceSpec.scala
@@ -1,6 +1,7 @@
 package com.example.offers
 
 import cats.effect.IO
+import io.circe.Json
 import org.http4s._
 import org.http4s.implicits._
 import org.scalatest.{Matchers, WordSpec}
@@ -18,6 +19,14 @@ class OffersServiceSpec extends WordSpec with Matchers {
       }
 
     }
+
+    "it receives a request to create a new offer" should {
+
+      "return a 201 status" in {
+        uriReturns201()
+      }
+
+    }
   }
 
   private val returnOffer: Response[IO] = {
@@ -25,8 +34,17 @@ class OffersServiceSpec extends WordSpec with Matchers {
     WebServer.offersService.orNotFound(getOffer).unsafeRunSync()
   }
 
+  private val createOffer: Response[IO] = {
+    val postOffer: Request[IO] = Request[IO](Method.POST, Uri.uri("/"))
+      .withBody(Json.obj()).unsafeRunSync()
+    WebServer.offersService.orNotFound(postOffer).unsafeRunSync()
+  }
+
   private def uriReturns200() =
     returnOffer.status shouldBe Status.Ok
+
+  private def uriReturns201() =
+    createOffer.status shouldBe Status.Created
 
   private def uriReturnsOffer() = {
     val expected = """{"_id":"1","desc":"This is a test","price":100,"currency":"GBP"}"""
