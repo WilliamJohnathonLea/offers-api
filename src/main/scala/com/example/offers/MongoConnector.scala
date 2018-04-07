c8f4663c9c81339824bc2c45bd3037bab86211d5
William Lea <wjlea93@gmail.com>
2018-04-07 13:02:41 +0100

    MongoConnector hooked up to mongo

--- src/main/scala/com/example/offers/MongoConnector.scala
+++ src/main/scala/com/example/offers/MongoConnector.scala
@@ -1,15 +1,79 @@
 package com.example.offers
 
 import cats.effect.IO
+import org.bson.codecs.configuration.CodecRegistries.{fromProviders, fromRegistries}
+import org.mongodb.scala.bson.codecs.DEFAULT_CODEC_REGISTRY
+import org.mongodb.scala.bson.codecs.Macros._
+import org.mongodb.scala.model.Filters._
+import org.mongodb.scala.model.Updates._
+import org.mongodb.scala.result.UpdateResult
+import org.mongodb.scala.{Completed, FindObservable, MongoClient, MongoCollection, MongoDatabase, Observer, SingleObservable}
+
 
 object MongoConnector {
 
-  def insert(offer: Offer): IO[Offer] = {
-    IO(offer)
+  private val mongoClient: MongoClient = MongoClient()
+  private val codecRegistry = fromRegistries(fromProviders(classOf[Offer]), DEFAULT_CODEC_REGISTRY)
+
+  private lazy val db: MongoDatabase = mongoClient.getDatabase("offers-api").withCodecRegistry(codecRegistry)
+  private lazy val offersCollection: MongoCollection[Offer] = db.getCollection("offers")
+
+
+  // Internal API used for testing
+  private[offers] def dropDatabase(): Unit = {
+    db.drop().subscribe(new Observer[Completed] {
+      override def onError(e: Throwable): Unit = ()
+
+      override def onComplete(): Unit = ()
+
+      override def onNext(result: Completed): Unit = ()
+    })
   }
 
-  def retrieve(id: String): IO[Offer] = {
-    IO(Offer(id, "This is a test", 100, "GBP"))
+  def insert(offer: Offer): IO[Offer] = {
+
+    val observable: SingleObservable[Completed] = offersCollection.insertOne(offer)
+
+    IO async[Offer] { cb =>
+      observable.subscribe(new Observer[Completed] {
+        override def onNext(result: Completed): Unit = cb(Right(offer))
+
+        override def onError(e: Throwable): Unit = cb(Left(e))
+
+        override def onComplete(): Unit = ()
+      })
+    }
+
+  }
+
+  def retrieve(id: String): IO[Option[Offer]] = {
+
+    val observable: FindObservable[Offer] = offersCollection.find(equal("_id", id))
+
+    IO async[Option[Offer]] { cb =>
+      observable.subscribe(new Observer[Offer] {
+        override def onError(e: Throwable): Unit = cb(Left(e))
+
+        override def onComplete(): Unit = cb(Right(None))
+
+        override def onNext(result: Offer): Unit = cb(Right(Some(result)))
+      })
+    }
+  }
+
+  def expire(id: String): IO[Unit] = {
+
+    val observable: SingleObservable[UpdateResult] = offersCollection.updateOne(equal("_id", id), set("hasExpired", true))
+
+    IO async { cb =>
+      observable.subscribe(new Observer[UpdateResult] {
+        override def onError(e: Throwable): Unit = cb(Left(e))
+
+        override def onComplete(): Unit = ()
+
+        override def onNext(result: UpdateResult): Unit = cb(Right())
+      })
+    }
   }
 
 }
--- src/test/scala/com/example/offers/MongoConnectorSpec.scala
+++ src/test/scala/com/example/offers/MongoConnectorSpec.scala
@@ -1,8 +1,12 @@
 package com.example.offers
 
-import org.scalatest.{Matchers, WordSpec}
+import org.scalatest.{BeforeAndAfterEach, Matchers, WordSpec}
 
-class MongoConnectorSpec extends WordSpec with Matchers {
+class MongoConnectorSpec extends WordSpec with Matchers with BeforeAndAfterEach {
+
+  override def beforeEach(): Unit = {
+    MongoConnector.dropDatabase()
+  }
 
   "MongoConnector" should {
 
@@ -15,9 +19,29 @@ class MongoConnectorSpec extends WordSpec with Matchers {
 
     "retrieve an offer" in {
 
-      val expected = Offer("1", "This is a test", 100, "GBP")
+      val offer = Offer("1", "This is a test", 100, "GBP")
+      val expected = Some(offer)
 
-      MongoConnector.retrieve("1").unsafeRunSync() shouldBe expected
+      val result = for {
+        o <- MongoConnector.insert(offer)
+        res <- MongoConnector.retrieve(o._id)
+      } yield res
+
+      result.unsafeRunSync() shouldBe expected
+    }
+
+    "expire an offer" in {
+
+      val offer = Offer("1", "This is a test", 100, "GBP")
+      val expected: Option[Offer] = Some(offer.copy(hasExpired = true))
+
+      val result = for {
+        o <- MongoConnector.insert(offer)
+        _ <- MongoConnector.expire(o._id)
+        res <- MongoConnector.retrieve(o._id)
+      } yield res
+
+      result.unsafeRunSync() shouldBe expected
     }
 
   }
